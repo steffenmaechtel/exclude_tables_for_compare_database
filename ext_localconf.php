ea95bb3e46f4d6b50a4b66737c9860d817661941
Steffen Maechtel <info@steffen-maechtel.de>
2018-10-18 18:54:00 +0200

    add support for typo3 7.x

--- ext_localconf.php
+++ ext_localconf.php
@@ -3,11 +3,11 @@
  * @author Steffen Maechtel <info@steffen-maechtel.de>
  */
 
-if (version_compare(TYPO3_version, '8.7.0', '>=')) {
+if (version_compare(TYPO3_version, '8.0.0', '>=')) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\Schema\\ConnectionMigrator'] = array(
         'className' => 'SteffenMaechtel\\ExcludeTablesForCompareDatabase\\Xclass\\FilterConnectionMigrator'
     );
-} else if (version_compare(TYPO3_version, '6.2.0', '>=') && version_compare(TYPO3_version, '6.2.999', '<')) {
+} else if (version_compare(TYPO3_version, '6.2.0', '>=') && version_compare(TYPO3_version, '7.6.999', '<')) {
     $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Install\\Service\\SqlSchemaMigrationService'] = array(
         'className' => 'SteffenMaechtel\\ExcludeTablesForCompareDatabase\\Xclass\\FilterSqlSchemaMigrationService'
     );
