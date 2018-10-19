5a3632a607e0b80f349de9995cdb58107ce915ba
Steffen Maechtel <info@steffen-maechtel.de>
2018-10-19 19:29:09 +0200

    make ext_localconf/ext_tables more typo3 like

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,13 +2,16 @@
 /**
  * @author Steffen Maechtel <info@steffen-maechtel.de>
  */
+defined('TYPO3_MODE') || die('Access denied.');
 
-if (version_compare(TYPO3_version, '8.7.0', '>=')) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\Schema\\ConnectionMigrator'] = array(
-        'className' => 'SteffenMaechtel\\ExcludeTablesForCompareDatabase\\Xclass\\FilterConnectionMigrator'
-    );
-} else if (version_compare(TYPO3_version, '6.2.0', '>=') && version_compare(TYPO3_version, '6.2.999', '<')) {
-    $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Install\\Service\\SqlSchemaMigrationService'] = array(
-        'className' => 'SteffenMaechtel\\ExcludeTablesForCompareDatabase\\Xclass\\FilterSqlSchemaMigrationService'
-    );
-}
+call_user_func(function () {
+    if (version_compare(TYPO3_version, '8.7.0', '>=')) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Core\\Database\\Schema\\ConnectionMigrator'] = array(
+            'className' => 'SteffenMaechtel\\ExcludeTablesForCompareDatabase\\Xclass\\FilterConnectionMigrator'
+        );
+    } else if (version_compare(TYPO3_version, '6.2.0', '>=') && version_compare(TYPO3_version, '6.2.999', '<')) {
+        $GLOBALS['TYPO3_CONF_VARS']['SYS']['Objects']['TYPO3\\CMS\\Install\\Service\\SqlSchemaMigrationService'] = array(
+            'className' => 'SteffenMaechtel\\ExcludeTablesForCompareDatabase\\Xclass\\FilterSqlSchemaMigrationService'
+        );
+    }
+});
--- ext_tables.php
+++ ext_tables.php
@@ -2,3 +2,4 @@
 /**
  * @author Steffen Maechtel <info@steffen-maechtel.de>
  */
+defined('TYPO3_MODE') || die('Access denied.');
